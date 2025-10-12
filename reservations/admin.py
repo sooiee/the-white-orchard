6583ec9cd0b5378dd65edaba08e9d13403a57bee
sooiee <217239261+sooiee@users.noreply.github.com>
2025-10-12 18:05:11 +0100

    Configure admin panel and add sample data

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import TimeSlot, Reservation, MenuItem
 
-# Register your models here.
+@admin.register(TimeSlot)
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('time', 'is_active')
+    list_filter = ('is_active',)
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'date', 'time_slot', 'number_of_guests', 'status', 'created_at')
+    list_filter = ('status', 'date', 'time_slot')
+    search_fields = ('customer_name', 'customer_email', 'customer_phone')
+    ordering = ('-date',)
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'is_available')
+    list_filter = ('category', 'is_available')
+    search_fields = ('name', 'description')
--- reservations/models.py
+++ reservations/models.py
@@ -61,3 +61,4 @@ class MenuItem(models.Model):
     
     def __str__(self):
         return self.name
+    
