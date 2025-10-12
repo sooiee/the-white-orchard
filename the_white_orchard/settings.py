bf680411804ae97fe368ad377f55f31bd9bc9c91
sooiee <217239261+sooiee@users.noreply.github.com>
2025-10-12 18:30:40 +0100

    Create static files structure and CSS

--- the_white_orchard/settings.py
+++ the_white_orchard/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
