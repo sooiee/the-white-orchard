5dd2e39944eff8e6a2e22a5524146c303b3ec9c1
sooiee <217239261+sooiee@users.noreply.github.com>
2025-10-13 10:18:18 +0100

    Implement user registration, login, and logout functionalities with success messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .forms import RegisterForm, LoginForm
 
-# Create your views here.
+
+def register(request):
+    """User registration"""
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f'Welcome {user.username}! Your account has been created.')
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def user_login(request):
+    """User login"""
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome back, {username}!')
+                return redirect('home')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def user_logout(request):
+    """User logout"""
+    logout(request)
+    messages.info(request, 'You have been logged out successfully.')
+    return redirect('home')
