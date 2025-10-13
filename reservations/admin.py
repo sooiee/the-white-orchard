eb8ff2796b0cdbb0ea953ff2e48de40f41b8ad6c
sooiee <217239261+sooiee@users.noreply.github.com>
2025-10-13 10:25:16 +0100

    Add Enquiry model admin interface with fieldsets and display options

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import TimeSlot, Reservation, MenuItem
+from .models import TimeSlot, Reservation, MenuItem, Enquiry
+
 
 @admin.register(TimeSlot)
 class TimeSlotAdmin(admin.ModelAdmin):
     list_display = ('time', 'is_active')
     list_filter = ('is_active',)
+
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
@@ -12,9 +14,42 @@ class ReservationAdmin(admin.ModelAdmin):
     list_filter = ('status', 'date', 'time_slot')
     search_fields = ('customer_name', 'customer_email', 'customer_phone')
     ordering = ('-date',)
+    
+    fieldsets = (
+        ('Customer Information', {
+            'fields': ('customer_name', 'customer_email', 'customer_phone')
+        }),
+        ('Booking Details', {
+            'fields': ('date', 'time_slot', 'number_of_guests', 'special_requests')
+        }),
+        ('Status', {
+            'fields': ('status',)
+        }),
+    )
+
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'is_available')
     list_filter = ('category', 'is_available')
     search_fields = ('name', 'description')
+
+
+@admin.register(Enquiry)
+class EnquiryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_at', 'is_responded')
+    list_filter = ('is_responded', 'created_at')
+    search_fields = ('name', 'email', 'subject', 'message')
+    ordering = ('-created_at',)
+    
+    fieldsets = (
+        ('Contact Information', {
+            'fields': ('name', 'email', 'phone')
+        }),
+        ('Enquiry Details', {
+            'fields': ('subject', 'message')
+        }),
+        ('Admin', {
+            'fields': ('is_responded', 'admin_notes')
+        }),
+    )
